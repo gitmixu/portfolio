224a7c514116b05be8d60a52451de95c336569d0
Mixu <saviojamicaela@gmail.com>
2024-05-05 12:41:22 +0300

    kukat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import redF from './imgs/3D-red.png'
+import yellowF from './imgs/3D-yellow.png'
 
 const App = () => {
 
   return (
     <div className='main'>
-      <h1>DevsNDesigns</h1>
-      <p>sivut tulossa!</p>
+      <div className="flowers">
+        <img className='red' src={redF} alt="" />
+        <img className='yellow' src={yellowF} alt="" />
+      </div>
+      <div className="title">
+        <h1>Devs<span>N</span></h1>
+        <h1 className='d'>Designs</h1>
+      </div>
+      <div className="notification">
+        <p>sivut tulossa !</p>
+      </div>
     </div>
   )
 }
