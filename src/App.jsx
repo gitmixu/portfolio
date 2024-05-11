d1543e3c3406fa53c71a8eedaf3c5bfb81417012
Mixu <saviojamicaela@gmail.com>
2024-05-11 14:21:56 +0300

    vaihtoehtoinen sivu

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import { useState } from 'react'
-import data from './data.json'
-import Questions from './components/Questions/Questions'
-import Home from './Pages/Home/Home'
-import Info from './Pages/Info/Info'
-import Contacts from './Pages/Contacts/Contacts'
+import Head from './Components/Head/Head'
+import MyServices from './Components/MyServices/MyServices'
 
 const App = () => {
-  const [question, setQuestion] = useState(0)
-  const [info, setInfo] = useState(0)
-
-  const  handleSelection = ( selection ) => {
-    setQuestion(1)
-    setInfo(selection)
-  }
-
-  const setPage = ( page ) => {
-    setQuestion(page)
-    setInfo(0)
-  }
 
   return (
     <div className='main'>
-      <div className="logo"><h1>DevsNDesigns</h1></div>
-        <Questions number={question} infoColor={info}setpage={setPage} />
-      
-
-
-        { question === 0 && <Home selectionControl={handleSelection} infoControl={setInfo} /> }
-        { question === 1 && <Info showInfo={info}  /> }
-        { question === 2 && <Contacts /> }
+      <Head />
+      <MyServices />
     </div>
   )
 }
