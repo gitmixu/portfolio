0133c648cd8ae81d157a9b2702ef71397f7b1a28
Mixu <saviojamicaela@gmail.com>
2024-06-12 00:54:14 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
-/* COMPONENTS */
-import Navbar from './components/Navbar/Navbar';
-
-/* PAGES */
-import Home from './Pages/Home/Home';
-import Servs from './pages/Servs/Servs';
-
 
 const App = () => {
 
   return (
-    <Router>
-      <Navbar />
-      <div className='main'>
-        <Routes>
-          <Route path='*' element={<Home/>}/>
-          <Route path='palvelut' element={<Servs/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <div className="main">
+      <p>Sivut tulossa pian</p>
+      <h1>DevsnDesigns</h1>
+    </div>
   )
 }
 
